ed5161f0d013776fd2e5df8062b6cdbb8b434d39
854292 <854292@SAIT227683IT.ACDM.DS.SAIT.CA>
2021-11-16 21:45:08 -0700

    Cloned Demo 10 Created a clone of demo 10 for this lab.

---
+++ src/java/filters/AuthenticationFilter.java
@@ -0,0 +1,50 @@
+
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+       
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+        
+        String email = (String) session.getAttribute("email");
+        
+        if(email == null)
+        {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.sendRedirect("login");
+            return;
+        }
+        // This will call upon either the nex filter in the chain or 
+        // or it will load the requested servlet.
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+   
+}
